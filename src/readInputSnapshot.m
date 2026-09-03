d69cc1088f1df710ee972819e952f3666a46358e
explorerzkb <explorerzkb@gmail.com>
2026-09-03 20:12:53 +0800

    feat(input): 增加双人按键状态读取

---
+++ src/readInputSnapshot.m
@@ -0,0 +1,33 @@
+function input = readInputSnapshot(fig, mappings)
+%READINPUTSNAPSHOT Convert the current pressed-key set into game actions.
+
+if ~isgraphics(fig)
+    keys = {};
+else
+    keys = getappdata(fig, 'pressedKeys');
+    if isempty(keys)
+        keys = {};
+    end
+end
+
+input.rawKeys = keys;
+input.player(1) = playerActions(keys, mappings.player1);
+primary = playerActions(keys, mappings.player2);
+alternate = playerActions(keys, mappings.player2Alternate);
+input.player(2).left = primary.left || alternate.left;
+input.player(2).right = primary.right || alternate.right;
+input.player(2).jump = primary.jump || alternate.jump;
+input.pause = hasKey(keys, mappings.pause);
+input.reset = hasKey(keys, mappings.reset);
+input.quit = hasKey(keys, mappings.quit);
+end
+
+function actions = playerActions(keys, mapping)
+actions.left = hasKey(keys, mapping.left);
+actions.right = hasKey(keys, mapping.right);
+actions.jump = hasKey(keys, mapping.jump);
+end
+
+function tf = hasKey(keys, key)
+tf = any(strcmpi(keys, char(key)));
+end
